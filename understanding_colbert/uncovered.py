8c8c204bb0a850c1ed7833aa28fec9f589186fe0
Siddhant Chaudhary <urssidd@gmail.com>
2024-03-25 03:22:14 +0530

    Adding more steps to the uncovered file

--- understanding_colbert/uncovered.py
+++ understanding_colbert/uncovered.py
@@ -17,6 +17,8 @@ from colbert.infra.launcher import Launcher
 from colbert.infra.config import BaseConfig, RunConfig, RunSettings
 from colbert.indexing.collection_indexer import CollectionIndexer
 from colbert.infra.launcher import print_memory_stats
+import faiss
+from colbert.indexing.codecs.residual import ResidualCodec
 
 dataroot = 'downloads/lotte'
 dataset = 'lifestyle'
@@ -154,6 +156,8 @@ assert local_sample_embs.half() == local_sample_embs
 
 avg_doclen_est = avg_doclen_est
 
+## _sample_embeddings call ends here
+
 num_passages = len(encoder.collection)
 encoder.num_embeddings_est = num_passages * avg_doclen_est
 encoder.num_partitions = int(2 ** np.floor(np.log2(16 * np.sqrt(encoder.num_embeddings_est))))
@@ -169,6 +173,220 @@ encoder._save_plan()
 #     encoder.train(shared_lists) # Trains centroids from selected passages
 if encoder.rank > 0:
     pass
+
+## sample, heldout = self._concatenate_and_split_sample()
+## the _concatenate_and_split_sample call
+sample = torch.empty(encoder.num_sample_embs, encoder.config.dim, dtype=torch.float16)
+offset = 0
+# # this for loop just unpacks the saved embeddings into a matrix
+# # for our case, nranks is just 1
+# for r in range(encoder.nranks):
+#     sub_sample_path = os.path.join(self.config.index_path_, f'sample.{r}.pt')
+#     sub_sample = torch.load(sub_sample_path)
+#     os.remove(sub_sample_path)
+#
+#     endpos = offset + sub_sample.size(0)
+#     sample[offset:endpos] = sub_sample
+#     offset = endpos
+offset = 0
+r = 0       # nranks = 1
+sub_sample_path = os.path.join(encoder.config.index_path_, f'sample.{r}.pt')
+sub_sample = torch.load(sub_sample_path)    # the saved tensor
+os.remove(sub_sample_path)
+
+endpos = offset + sub_sample.size(0)
+sample[offset:endpos] = sub_sample
+offset = endpos
+
+assert endpos == sample.size(0), (endpos, sample.size())
+
+# randomly permute the sample
+sample = sample[torch.randperm(sample.size(0))]
+
+heldout_fraction = 0.05
+heldout_size = int(min(heldout_fraction * sample.size(0), 50_000))
+sample, sample_heldout = sample.split([sample.size(0) - heldout_size, heldout_size], dim=0)
+
+assert sample_heldout.size()[0] + sample.size()[0] == encoder.num_sample_embs
+sample, sample_heldout = sample, sample_heldout
+
+# _concatenate_and_split_sample call ends here
+# centroids = self._train_kmeans(sample, shared_lists)
+# the _train_kmeans call
+if encoder.use_gpu:
+    torch.cuda.empty_cache()
+
+args_ = [encoder.config.dim, encoder.num_partitions, encoder.config.kmeans_niters]
+
+# do_fork_for_faiss is not true
+# so we only include the else block of the function here
+args_ = args_ + [[[sample]]]    # args now consists of 4 things
+
+# centroids = compute_faiss_kmeans(*args_)
+# the compute_faiss_kmeans call
+dim, num_partitions, kmeans_niters, shared_lists = args_[0], args_[1], args_[2], args_[3]
+# so shared_lists here is [sample], i.e a list containing the sample
+return_value_queue=None
+
+use_gpu = torch.cuda.is_available()
+kmeans = faiss.Kmeans(dim, num_partitions, niter=kmeans_niters, gpu=use_gpu, verbose=True, seed=123)
+sample = shared_lists[0][0]     # our sample
+sample = sample.float().numpy()
+kmeans.train(sample)
+
+centroids = torch.from_numpy(kmeans.centroids)
+# compute_faiss_kmeans call ends here
+
+centroids = torch.nn.functional.normalize(centroids, dim=-1)
+if encoder.use_gpu:
+    centroids = centroids.half()
+else:
+    centroids = centroids.float()
+## _train_kmeans call ends here
+del sample      # delete the sample set
+
+# bucket_cutoffs, bucket_weights, avg_residual = self._compute_avg_residual(centroids, heldout)
+## the _compute_avg_residual call
+# the average is computed over the heldout set
+heldout = sample_heldout
+compressor = ResidualCodec(config=encoder.config, centroids=centroids, avg_residual=None)
+assert compressor.use_gpu > 0
+compressor.centroids
+assert compressor.dim == encoder.config.dim
+assert compressor.nbits == encoder.config.nbits
+assert compressor.avg_residual == None
+assert compressor.bucket_cutoffs == None
+assert compressor.bucket_weights == None
+compressor.arange_bits # = torch.arange(0, compressor.nbits, device='cuda' if compressor.use_gpu else 'cpu', dtype=torch.uint8)
+compressor.reversed_bit_map
+
+mask = (1 << compressor.nbits) - 1  # all 1's, number of 1's = compressor.nbits
+reversed_bit_map = []
+for i in range(256):
+    # The reversed byte
+    z = 0
+    for j in range(8, 0, -compressor.nbits): # [8, 6, 4, 2]
+        # Extract a subsequence of length n bits
+        x = (i >> (j - compressor.nbits)) & mask    # j - compressor.nbits is in [6, 4, 2, 0]
+
+        # Reverse the endianness of each bit subsequence (e.g. 10 -> 01)
+        y = 0
+        for k in range(compressor.nbits - 1, -1, -1):
+            y += ((x >> (compressor.nbits - k - 1)) & 1) * (2 ** k)
+
+        # Set the corresponding bits in the output byte
+        z |= y
+        if j > compressor.nbits:
+            z <<= compressor.nbits
+    reversed_bit_map.append(z)
+
+assert compressor.decompression_lookup_table is None
+ResidualCodec.decompress_residuals  # decompress_residuals.cpp, decompress_residuals.cu
+ResidualCodec.packbits # pacbits.cpp, packbits.cu 
+
+# end of ResidualCodec constructor here
+# heldout_reconstruct = compressor.compress_into_codes(heldout, out_device='cuda' if self.use_gpu else 'cpu')
+## the compress_into_codes call
+embs = heldout
+out_device='cuda' if encoder.use_gpu else 'cpu'
+
+## output on a single batch
+bsize = (1 << 29) // compressor.centroids.size(0)
+single_batch = embs.split(bsize)[0]
+single_batch.size()     # [8192, 128]
+single_batch.T
+single_batch.T.size()   # [128, 8192]
+compressor.centroids @ single_batch.T.cuda().half() # all possible inner products
+(compressor.centroids @ single_batch.T.cuda().half()).size()    # [num_centroids, 8192]
+(compressor.centroids @ single_batch.T.cuda().half()).max(dim=0)
+indices = (compressor.centroids @ single_batch.T.cuda().half()).max(dim = 0).indices.to(device = out_device)
+assert indices.size()[0] == 8192    # for each embedding in the batch, we got the index of the closest centroid
+
+# output on all batches
+codes = []
+bsize = (1 << 29) // compressor.centroids.size(0)
+for batch in embs.split(bsize):     # split sample_heldout into batches of size bsize
+    if compressor.use_gpu:
+        indices = (compressor.centroids @ batch.T.cuda().half()).max(dim=0).indices.to(device=out_device)
+    else:
+        indices = (compressor.centroids @ batch.T.cpu().float()).max(dim=0).indices.to(device=out_device)
+    codes.append(indices)
+
+# heldout reconstruct holds the indices of the closest centroid for each heldout embedding
+heldout_reconstruct = torch.cat(codes)      ## collect all the codes together
+## the compress_into_codes call ends here
+assert heldout_reconstruct.size()[0] == sample_heldout.size()[0]
+
+# heldout_reconstruct = compressor.lookup_centroids(heldout_reconstruct, out_device='cuda' if self.use_gpu else 'cpu')
+## the lookup_centroids call
+codes = heldout_reconstruct
+inner_centroids = []
+
+for batch in codes.split(1 << 20): # contains only one batch
+    if compressor.use_gpu:
+        inner_centroids.append(compressor.centroids[batch.cuda().long()].to(device=out_device))
+    else:
+        inner_centroids.append(compressor.centroids[batch.long()].to(device=out_device))
+# so, centroids contain the centroids closest to all the embeddings in the heldout set
+heldout_reconstruct = torch.cat(inner_centroids)
+
+## lookup_centroids call ends here
+if encoder.use_gpu:
+    heldout_avg_residual = heldout.cuda() - heldout_reconstruct
+else:
+    heldout_avg_residual = heldout - heldout_reconstruct
+
+# avg_residual is the average of all absolute values of heldout residuals over each dimension
+avg_residual = torch.abs(heldout_avg_residual).mean(dim=0).cpu()    # why take abs here?
+print([round(x, 3) for x in avg_residual.squeeze().tolist()])
+
+num_options = 2 ** encoder.config.nbits
+quantiles = torch.arange(0, num_options, device=heldout_avg_residual.device) * (1 / num_options)
+bucket_cutoffs_quantiles, bucket_weights_quantiles = quantiles[1:], quantiles + (0.5 / num_options)
+
+bucket_cutoffs = heldout_avg_residual.float().quantile(bucket_cutoffs_quantiles)
+bucket_weights = heldout_avg_residual.float().quantile(bucket_weights_quantiles)
+
+if encoder.verbose > 2:
+    print_message(
+        f"#> Got bucket_cutoffs_quantiles = {bucket_cutoffs_quantiles} and bucket_weights_quantiles = {bucket_weights_quantiles}")
+    print_message(f"#> Got bucket_cutoffs = {bucket_cutoffs} and bucket_weights = {bucket_weights}")
+
+bucket_cutoffs, bucket_weights, avg_residual = bucket_cutoffs, bucket_weights, avg_residual.mean()
+#_compute_avg_residual call ends here
+
+if encoder.verbose > 1:
+    print_message(f'avg_residual = {avg_residual}')
+
+codec = ResidualCodec(config=encoder.config, centroids=centroids, avg_residual=avg_residual, bucket_cutoffs=bucket_cutoffs, bucket_weights=bucket_weights)
+# encoder.saver.save_codec(codec)
+# the save_code(codec) call
+# codec.save(index_path=encoder.saver.config.index_path_)
+# the codec.save call
+index_path = encoder.saver.config.index_path_
+assert codec.avg_residual is not None
+assert torch.is_tensor(codec.bucket_cutoffs), codec.bucket_cutoffs
+assert torch.is_tensor(codec.bucket_weights), codec.bucket_weights
+
+centroids_path = os.path.join(index_path, 'centroids.pt')
+avgresidual_path = os.path.join(index_path, 'avg_residual.pt')
+buckets_path = os.path.join(index_path, 'buckets.pt')
+
+torch.save(codec.centroids.half(), centroids_path)
+torch.save((codec.bucket_cutoffs, codec.bucket_weights), buckets_path)
+
+if torch.is_tensor(codec.avg_residual):
+    torch.save(codec.avg_residual, avgresidual_path)
+else:
+    torch.save(torch.tensor([codec.avg_residual]), avgresidual_path)
+
+# codec.save call ends here
+# save_codec call ends here
+# train call ends here
+
+## self.train(shared_lists) call ends here
+## self.index()
+
 
 torch_context.__exit__(None, None, None)
 

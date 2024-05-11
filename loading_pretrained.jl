647d44f1e7afb8e450c5523e929f51439d8b13f0
Siddhant Chaudhary <urssidd@gmail.com>
2024-05-11 17:45:13 +0530

    Adding more steps to load the model (BERT + linear layer) in julia.

--- loading_pretrained.jl
+++ loading_pretrained.jl
@@ -1,11 +1,49 @@
 ENV["OPENAI_API_KEY"] = ""
 
 using Flux, Transformers, CUDA
+include("config.jl")
+
+# paths and names
+dataroot = "downloads/lotte"
+dataset = "lifestyle"
+datasplit = "dev"
+
+nbits = 2   # encode each dimension with 2 bits
+kmeans_niters = 20  # 20 iterations of kmeans
+doc_maxlen = 300   # truncate passages at 300 tokens
+
+checkpoint = "downloads/colbertv2.0"
+index_root = "experiments/notebook/indexes"
+index_name = "$dataset.$datasplit.$nbits"*"bits"
+index_path = joinpath(index_root, index_name)
+
+# setting up a config
+config = ColBERTConfig(
+    RunSettings(
+        experiment="notebook",
+        name="2024-03/29/16.34.17",
+    ),
+    TokenizerSettings(),
+    ResourceSettings(
+        checkpoint=checkpoint,
+        collection="downloads/lotte/lifestyle/dev/collection.tsv",
+        index_name=index_name,
+    ),
+    DocSettings(
+        doc_maxlen=doc_maxlen,
+    ),
+    QuerySettings(),
+    IndexingSettings(
+        nbits=nbits,
+        kmeans_niters=kmeans_niters,
+    ),
+    SearchSettings(),
+)
 
 # check if CUDA devices are available
 CUDA.devices()
 
-const PRETRAINED_MODEL = "colbert-ir/colbertv2.0"
+const PRETRAINED_MODEL = "downloads/colbertv2.0"                            # relative path to where the model is downloaded
 
 colbert_config = Transformers.load_config(PRETRAINED_MODEL)
 colbert_tokenizer = Transformers.load_tokenizer(PRETRAINED_MODEL)
--- steps_for_gpu_server.md
+++ steps_for_gpu_server.md
@@ -2,3 +2,6 @@ tmux -u
 conda activate colbert
 cd colbert_project_gsoc
 CUDA_VISIBLE_DEVICES=0 python3
+
+## move code to GPU directory
+scp -J chaudhary@cmi -r ./*  chaudhary@cmigpu:/home/chaudhary/colBert_project_gsoc/
